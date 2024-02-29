4caefe5eebd2c071b71a9072fbc5149f13dfc117
mustafanafia <55895079+mustafanafia@users.noreply.github.com>
2024-02-29 13:51:50 -0500

    Assignment 1

---
+++ RepeatAdditionQuiz.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+   
+    public class RepeatAdditionQuiz {
+     public static void main(String[] args) {
+         
+
+
+for(int i = 1; i <= 200; i++) {
+    
+    if (i % 2 == 0 && i % 3 == 0) {
+        System.out.print(i + " ");
+        
+    }
+}
+ 
+}
+}
+
+
+
+
+
